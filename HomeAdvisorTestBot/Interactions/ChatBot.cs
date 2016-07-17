a150b0263ad41352692700e8d804aa6d9491665d
Levi Fuller <lfuller941@gmail.com>
2016-07-17 11:06:44 -0600

    Carousel working - Needs logic

--- HomeAdvisorTestBot/Interactions/ChatBot.cs
+++ HomeAdvisorTestBot/Interactions/ChatBot.cs
@@ -1,4 +1,5 @@
-﻿using HomeAdvisorTestBot.NaturalLanguage;
+﻿using HomeAdvisorTestBot.Models;
+using HomeAdvisorTestBot.NaturalLanguage;
 using Microsoft.Bot.Connector;
 using System;
 using System.Collections.Generic;
@@ -11,35 +12,78 @@ namespace HomeAdvisorTestBot.Interactions {
 
         public async Task<Activity> HandleChat(Activity message) {
             Activity replyToConversation;
-            return message.CreateReply(await GetReplyFromLuis(message.Text));
-            if (!await DidSendGreeting(message)) {
-                replyToConversation = GetGreetingReply(message);
+            replyToConversation = message.CreateReply(await GetReplyFromLuis(message.Text));
+
+            replyToConversation.Recipient = message.From;
+            replyToConversation.Type = "message";
+            replyToConversation.AttachmentLayout = "carousel";
+            replyToConversation.Attachments = new List<Attachment>();
+            Dictionary<string, string> cardContentList = new Dictionary<string, string>();
+            cardContentList.Add("PigLatin", "https://stanleyyelnats.wikispaces.com/file/view/jhan171l2.JPG/30641276/361x347/jhan171l2.JPG");
+            cardContentList.Add("Pork Shoulder", "https://bigoven-res.cloudinary.com/image/upload/t_recipe-256/slow-roasted-glazed-pork-shoulder-1319864.jpg");
+            cardContentList.Add("Bacon", "http://images.bigoven.com/image/upload/t_recipe-256/smoked-bacon-wrappped-meat-loaf.jpg");
+            foreach (KeyValuePair<string, string> cardContent in cardContentList) {
+                List<CardImage> cardImages = new List<CardImage>();
+                cardImages.Add(new CardImage(url: cardContent.Value));
+                List<CardAction> cardButtons = new List<CardAction>();
+                CardAction plButton = new CardAction() {
+                    Value = $"https://en.wikipedia.org/wiki/{cardContent.Key}",
+                    Type = "openUrl",
+                    Title = "WikiPedia Page"
+                };
+                cardButtons.Add(plButton);
+                HeroCard plCard = new HeroCard() {
+                    Title = $"I'm a hero card about {cardContent.Key}",
+                    Subtitle = $"{cardContent.Key} Wikipedia Page",
+                    Images = cardImages,
+                    Buttons = cardButtons
+                };
+                Attachment plAttachment = plCard.ToAttachment();
+                replyToConversation.Attachments.Add(plAttachment);
             }
-            else {
-                replyToConversation = GetSecondReply(message);
-            }
+            replyToConversation.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+
+
+
+
             return replyToConversation;
         }
 
         private async Task<string> GetReplyFromLuis(string messageText) {
             string toReturn = string.Empty;
             HomeAdvisorLuis requestDetails = await LuisClient.ParseUserInput(messageText);
-            switch (requestDetails.intents[0].intent) {
-                case "RequestService":
-                    toReturn = $"I see you want to request a service to {GetRequestedService(requestDetails.entities)} your {GetRequestedTarget(requestDetails.entities)} - Is that correct?";
-                    break;
+            var request = new ServiceRequest(requestDetails);
+            toReturn = ConstructReplyFromServiceRequest(request);
+
+            return toReturn;
+        }
+
+
+        private string ConstructReplyFromServiceRequest(ServiceRequest request) {
+            string toReturn = "You need help with ";
+
+            // TODO: Save states based on data retrieved (e.g. if service == null, but day exists, store the desired day and request the service, etc.)
+            if (request.Service != null) {
+                if (request.Service.EndsWith("ing", StringComparison.OrdinalIgnoreCase))
+                    request.Service += "ing";
+                toReturn += request.Service;
+                
+                if (request.Target != null) {
+                    toReturn += $" your {request.Target}";
+                }
+
+                if (request.Day != null) {
+                    toReturn += $" on {request.Day}";
+                }
+            }
+            else {
+                toReturn = "Sorry, we couldn't find any related service. Try something like 'I need to remodel my kitchen' or 'I need my AC repaired'";
             }
             return toReturn;
-
         }
 
-        private string GetRequestedService(HomeAdvisorTestBot.NaturalLanguage.Entity[] entities) {
-            return entities.FirstOrDefault(x => x.type == "RequestedService").entity;
-        }
+       
 
-        private string GetRequestedTarget(HomeAdvisorTestBot.NaturalLanguage.Entity[] entities) {
-            return entities.FirstOrDefault(x => x.type == "Target").entity;
-        }
 
         private Activity GetGreetingReply(Activity messageFromUser) {
             Activity toReturn = messageFromUser.CreateReply("Hello there!");
---
+++ HomeAdvisorTestBot/Models/ServiceRequest.cs
@@ -0,0 +1,39 @@
+﻿using HomeAdvisorTestBot.NaturalLanguage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeAdvisorTestBot.Models {
+    public class ServiceRequest {
+        public ServiceRequest(HomeAdvisorLuis luisModel) {
+            foreach (var entity in luisModel.entities) {
+                switch (entity.type) {
+                    case "RequestedService":
+                        this.Service = entity.entity;
+                        break;
+                    case "Target":
+                        this.Target = entity.entity;
+                        break;
+                    case "RequestedDay":
+                        this.Day = entity.entity;
+                        break;
+                    case "RequestedTime":
+                        this.Time = entity.entity;
+                        break;
+                    case "RequestedTimeOfDay":
+                        this.TimeOfDay = entity.entity;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public string Service { get; set; }
+        public string Target { get; set; }
+        public string Time { get; set; }
+        public string TimeOfDay { get; set; }
+        public string Day { get; set; }
+    }
+}
